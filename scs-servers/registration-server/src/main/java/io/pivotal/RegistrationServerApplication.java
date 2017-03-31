22144e9decf17598bad2ab72d446df4587bccc5b
sdeeg-pivotal <sdeeg@pivotal.io>
2017-03-31 01:35:26 -0700

    added

---
+++ scs-servers/registration-server/src/main/java/io/pivotal/RegistrationServerApplication.java
@@ -0,0 +1,14 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistrationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServerApplication.class, args);
+	}
+}
