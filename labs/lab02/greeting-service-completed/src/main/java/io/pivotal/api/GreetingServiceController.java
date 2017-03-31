9f6b8dc96e8e2a1a0ae7b8405e66a6e01a13dc02
sdeeg-pivotal <sdeeg@pivotal.io>
2017-03-30 17:51:36 -0700

    added metrics, formatting

--- labs/lab02/greeting-service-completed/src/main/java/io/pivotal/api/GreetingServiceController.java
+++ labs/lab02/greeting-service-completed/src/main/java/io/pivotal/api/GreetingServiceController.java
@@ -1,5 +1,6 @@
 package io.pivotal.api;
 
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +9,17 @@ import io.pivotal.dom.Greeting;
 @RestController
 public class GreetingServiceController {
 	private Greeting greeting;
+	private final CounterService counterService;
 	
-	public GreetingServiceController(Greeting greeting) {
+	public GreetingServiceController(Greeting greeting, CounterService counterService) {
 		super();
 		this.greeting = greeting;
+		this.counterService = counterService;
 	}
 
 	@GetMapping("/greeting")
 	public Greeting greeting() {
+		counterService.increment("greeting.service.invoked");
 	    return greeting;
 	}
 }
